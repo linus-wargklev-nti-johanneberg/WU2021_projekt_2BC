64ca1ec77d8841785a7256cf67b00d95790211d2
linus-wargklev-nti-johanneberg <linus.wargklev@elev.ga.ntig.se>
2021-03-02 13:19:59 +0100

    Tisdag 23/2
    Idag märkte jag att jag har gjort en väldigt rörig kod som inte är organiserad och bra för den hemsidan jag ska koda. Så därför är jag tvungen att lägga mobil-sidan på hyllan ett litet tag och komma tillbaka till den när jag har kodat klart sidan för desktop. Idag började jag därför koda hemsidan för desktop, som gick mycket bättre eftersom att jag hade organiserat den delen på ett mycket bättre sätt. Det gick bra att koda vissa rubriker och texter som finns inne i de bilderna jag har på startsidan. Det som kunde gått bättre var det som hände i början på lektionen, att jag inte fick någon ordning på min kod och får därför börja om/rensa kod på min mobilsida när jag är färdig med min sida för desktop.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector(".menu-button").addEventListener("click", showMenu)
+
+// function showMenu(){
+    //let menu = document.querySelector(".navhamburg");
+    //menu.classList.toggle("show");
+//} 
